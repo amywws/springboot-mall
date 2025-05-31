1d9879139f4c90a97a55bcaba200c06c68aebc81
Amy Wang <amywws@gmail.com>
2025-05-31 17:01:14 +0800

    商品功能 - 實作查詢商品列表(查詢條件)

--- src/main/java/com/amy/springbootmall/dao/ProductDao.java
+++ src/main/java/com/amy/springbootmall/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.amy.springbootmall.dao;
 
+import com.amy.springbootmall.constant.ProductCategory;
 import com.amy.springbootmall.dto.ProductRequest;
 import com.amy.springbootmall.model.Product;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/amy/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/amy/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.amy.springbootmall.service.impl;
 
+import com.amy.springbootmall.constant.ProductCategory;
 import com.amy.springbootmall.dao.ProductDao;
 import com.amy.springbootmall.dto.ProductRequest;
 import com.amy.springbootmall.model.Product;
@@ -17,8 +18,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
